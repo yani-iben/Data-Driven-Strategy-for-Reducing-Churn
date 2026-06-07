986b0057a6cadf16e3500c6a9714cd7aa5dab9ff
“yani-iben” <yani.iben5@gmail.com>
2026-06-07 14:04:11 -0400

    updating icon

--- dashboard/app.py
+++ dashboard/app.py
@@ -9,7 +9,6 @@ from sqlalchemy import create_engine
 # ==========================================
 st.set_page_config(
     page_title="Executive Volunteer Analytics Platform",
-    page_icon=",
     layout="wide",
     initial_sidebar_state="collapsed"
 )
